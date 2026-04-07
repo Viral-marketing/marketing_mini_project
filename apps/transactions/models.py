538879675e6f6d17634da2125707d2528b99ca2b
gunung-kim <unge001@naver.com>
2026-04-07 16:42:36 +0900

    feat:transaction serializer 추가

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -18,9 +18,12 @@ class Transaction(models.Model):
         ("INTEREST", "이자"),
     ]
 
-    account = models.ForeignKey(Account,on_delete=models.CASCADE,related_name='transactions')
+    account = models.ForeignKey(Account,on_delete=models.SET_NULL,related_name='transactions',null=True)
+    # 계좌가 삭제되어도 거래기록은 남아있어야함
     transaction_amount = models.DecimalField(max_digits=10,decimal_places=2)
+    # 소수점 2자리까지 표시, 8자리 까지 표시가능(천만원)
     transaction_type = models.CharField(choices=TRANSACTION_TYPE,max_length=20,default="DEPOSIT")
+    # type으로 입금인지 출금인지 확인 출금이면 비지니스로직에서 '-' 추가 필요
     transaction_method = models.CharField(choices=TRANSACTION_METHOD,max_length=20,default="ATM")
     memo = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- apps/transactions/serializers.py
+++ apps/transactions/serializers.py
@@ -1,5 +1,28 @@
 from rest_framework import serializers
-
+from apps.transactions.models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
-    account =
+    transaction_type_display = serializers.CharField(source='get_transaction_type_display',read_only=True)
+    transaction_method_display = serializers.CharField(source='get_transaction_method_display',read_only=True)
+    class Meta:
+        model = Transaction
+        fields = [
+            'id',
+            'account',
+            'transaction_type',
+            'transaction_type_display',
+            # 응답용 transaction_type
+            'transaction_method',
+            'transaction_method_display',
+            # 응답용 transaction_method
+            'transaction_amount',
+            'memo',
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = ['id','account','created_at','updated_at']
+
+    def validate_transaction_amount(self,value):
+        if value <= 0:
+            raise serializers.ValidationError('거래금액은 0보다 커야합니다')
+        return value
