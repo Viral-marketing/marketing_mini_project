fe4643ab6d35704307e3c0b1a73f0cd913c66c7d
Yoojin Sohn <ysohn06@gmail.com>
2026-04-08 17:27:44 +0900

    style: Ruff 코드 스타일 적용 (따옴표 및 임포트 정렬)

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from apps.common.constants import BANK_CODES, ACCOUNT_TYPE
+
+from apps.common.constants import ACCOUNT_TYPE, BANK_CODES
+
 
 class Account(models.Model):
     user = models.ForeignKey(
-        'users.User',
+        "users.User",
         on_delete=models.CASCADE,
-        related_name='accounts'
+        related_name="accounts"
     )
     account_number = models.CharField(max_length=20, unique=True)
     bank_code = models.CharField(max_length=10, choices=BANK_CODES)
@@ -13,7 +15,7 @@ class Account(models.Model):
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     class Meta:
-        db_table = 'accounts'
+        db_table = "accounts"
 
     def __str__(self):
         return f"{self.bank_code} - {self.account_number}"
