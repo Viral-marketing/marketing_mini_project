e03dde5094846051dd1bb7fd35dcaa3da12e079e
gunung-kim <unge001@naver.com>
2026-04-08 16:51:44 +0900

    feat:transaction model 생성

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -1,1 +1,46 @@
-# Create your models here.
+from django.db import models
+from apps.accounts.models import Account
+from apps.common.constants import TRANSACTION_TYPE,TRANSACTION_METHOD
+
+class Transaction(models.Model):
+
+    # # 거래 타입
+    # TRANSACTION_TYPE = [
+    #     ("DEPOSIT", "입금"),
+    #     ("WITHDRAW", "출금"),
+    # ]
+    #
+    # # 거래 종류
+    # TRANSACTION_METHOD = [
+    #     ("ATM", "ATM 거래"),
+    #     ("TRANSFER", "계좌이체"),
+    #     ("AUTOMATIC_TRANSFER", "자동이체"),
+    #     ("CARD", "카드결제"),
+    #     ("INTEREST", "이자"),
+    # ]
+
+    account = models.ForeignKey(Account,on_delete=models.SET_NULL,related_name='transactions',null=True)
+    # 계좌가 삭제되어도 거래기록은 남아있어야함
+    transaction_amount = models.DecimalField(max_digits=20,decimal_places=2)
+    # 소수점 2자리까지 표시, 18자리 까지 표시가능
+    transaction_type = models.CharField(choices=TRANSACTION_TYPE,max_length=20,default="DEPOSIT")
+    # type으로 입금인지 출금인지 확인 출금이면 비지니스로직에서 '-' 추가 필요
+    transaction_method = models.CharField(choices=TRANSACTION_METHOD,max_length=20,default="ATM")
+    balance_after = models.DecimalField(max_digits=20,decimal_places=2,editable=False)
+    memo = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['account']),
+            models.Index(fields=['transaction_type','transaction_amount','transaction_method']),
+            models.Index(fields=['created_at']),
+        ]
+    def save(self,*args,**kwargs):
+        if self.account:
+            if self.transaction_type == 'DEPOSIT':
+                self.balance_after=self.account.balance+self.transaction_amount
+            else :
+                self.balance_after=self.account.balance-self.transaction_amount
