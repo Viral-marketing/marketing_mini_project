0806fcb5fd94c61b5f0923b254bbdc516b191c31
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-04-08 16:37:47 +0900

    feat:유저모델 정의

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,11 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
-    name = models.CharField()
-    email = models.EmailField()
+    name = models.CharField(max_length=20, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False, unique=True)
     password = models.CharField(max_length=128)
-    phone = models.IntegerField()
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=15, null=False, blank=False)
     updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name", "phone"]
+
+    class Meta:
+        db_table = "user"
+        verbose_name_plural = "users"
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return f"{self.name}{self.email}"
