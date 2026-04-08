72ac99be120cc77c57037c52d64810a9a36ea185
gunung-kim <unge001@naver.com>
2026-04-08 17:13:40 +0900

    chore:거래내역 모델정의 및 줄바꿈수정

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -19,14 +19,27 @@ class Transaction(models.Model):
     #     ("INTEREST", "이자"),
     # ]
 
-    account = models.ForeignKey(Account,on_delete=models.SET_NULL,related_name='transactions',null=True)
+    account = models.ForeignKey(Account,
+                                on_delete=models.SET_NULL,
+                                related_name='transactions',
+                                null=True
+                                )
     # 계좌가 삭제되어도 거래기록은 남아있어야함
     transaction_amount = models.DecimalField(max_digits=20,decimal_places=2)
     # 소수점 2자리까지 표시, 18자리 까지 표시가능
-    transaction_type = models.CharField(choices=TRANSACTION_TYPE,max_length=20,default="DEPOSIT")
+    transaction_type = models.CharField(choices=TRANSACTION_TYPE,
+                                        max_length=20,
+                                        default="DEPOSIT"
+                                        )
     # type으로 입금인지 출금인지 확인 출금이면 비지니스로직에서 '-' 추가 필요
-    transaction_method = models.CharField(choices=TRANSACTION_METHOD,max_length=20,default="ATM")
-    balance_after = models.DecimalField(max_digits=20,decimal_places=2,editable=False)
+    transaction_method = models.CharField(choices=TRANSACTION_METHOD,
+                                          max_length=20,
+                                          default="ATM"
+                                          )
+    balance_after = models.DecimalField(max_digits=20,
+                                        decimal_places=2,
+                                        editable=False
+                                        )
     memo = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -35,6 +48,10 @@ class Transaction(models.Model):
         ordering = ['-created_at']
         indexes = [
             models.Index(fields=['account']),
-            models.Index(fields=['transaction_type','transaction_amount','transaction_method']),
+            models.Index(fields=['transaction_type',
+                                 'transaction_amount',
+                                 'transaction_method'
+                                 ]
+                         ),
             models.Index(fields=['created_at']),
         ]
