2908099ca289650c2ee57abc78c68686081384e2
Yoojin Sohn <ysohn06@gmail.com>
2026-04-08 17:19:35 +0900

    feat: 계좌 모델 구현

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,15 +1,19 @@
 from django.db import models
-from apps.common.constants import BANK_CODES, ACCOUNT_TYPE  # 경로 확인 필요
+from apps.common.constants import BANK_CODES, ACCOUNT_TYPE
 
 class Account(models.Model):
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='accounts')
+    user = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='accounts'
+    )
     account_number = models.CharField(max_length=20, unique=True)
     bank_code = models.CharField(max_length=10, choices=BANK_CODES)
     account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPE)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
-class Meta:
-    db_table = 'accounts'
+    class Meta:
+        db_table = 'accounts'
 
-def __str__(self):
-    return f"{self.bank_code} - {self.account_number}"
+    def __str__(self):
+        return f"{self.bank_code} - {self.account_number}"
