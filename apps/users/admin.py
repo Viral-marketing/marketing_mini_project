da7653f7089d20f4b22f6702815e8ff29f040881
em <ererjomk5612@gmail.com>
2026-04-09 16:44:33 +0900

    feat: adminPage추가

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+from apps.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ("email", "name", "phone")
+    list_display = ("is_staff", "is_superuser", "is_active")
+    readonly_fields = ("is_staff", "is_superuser")
