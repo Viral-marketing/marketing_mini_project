46d77a581119ede97ac0936f2592772ceff05311
jihyeongh21-svg <jihyeongh21@gmail.com>
2026-04-10 14:51:12 +0900

    중간 저장

--- apps/users/services.py
+++ apps/users/services.py
@@ -4,12 +4,26 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
-
+# 유저 CRUD
 def create_user(email: str, name: str, password: str, phone: str):
     user = User(email=email, name=name, phone=phone)
     user.set_password(password)
     user.save()
     return user
+def read_user(user: User):
+    return {
+        "email": user.email,
+    }
+
+
+def update_user(user: User, email: str, name: str, phone: str):
+
+
+
+def delete_user(user: User):
+
+
+
 
 
 # 로그인 로직
@@ -18,7 +32,7 @@ def login_user(email: str, password: str):
 
     if user is None:
         raise exceptions.AuthenticationFailed("없는 계정입니다")
-    if not user.is_active:
+    if user.is_active == False:
         raise exceptions.AuthenticationFailed("탈퇴한 계정입니다")
 
     refresh = RefreshToken.for_user(user)
